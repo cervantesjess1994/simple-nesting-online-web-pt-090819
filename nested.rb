74b7ed8275943dd00879b272b0177bd61f67edd2
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-24 12:00:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,8 +64,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-  adding_matz[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -81,8 +79,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-
+     programmer_hash[:yukihiro_matsumoto] = {known_for:"Ruby", languages:["LISP", "C"]}
+     programmer_hash
 end
 
 def changing_alan
